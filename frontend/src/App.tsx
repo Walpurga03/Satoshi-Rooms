71988e593f00e4b4704fa9dffd11a6f31450ee99
Walpurga03 <alex.moik@proton.me>
2025-07-30 21:07:29 +0200

    Relay-Verbindungen & Management

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Layout from './components/Layout';
 import KeyManagement from './components/KeyManagement';
+import RelayManagement from './components/RelayManagement';
 
 function App() {
   return (
     <Layout>
       <h1>Nostr Groups Client</h1>
       <KeyManagement />
+      <RelayManagement />
     </Layout>
   );
 }
